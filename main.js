46268b914481302a31363851c535d86504d97a69
Emily Deans <emilyldeans@gmail.com>
2017-07-15 10:38:59 -0400

    Added inner HTML for screen and updated button formatting

---
+++ main.js
@@ -0,0 +1,34 @@
+// // variables for the 'clear' and 'screen' blocks
+// let screen = document.getElementById("screen");
+// let clear = document.getElementById("clear");
+// let buttonFunction = document.getElementByClass("button");
+//
+// // adding 'click' event listener to buttons
+// buttonFunction.addEventListener("click", buttonValue);
+//
+// function buttonValue() {
+//   for(let i = 0; i < )
+// }
+//
+//   let value = document.calcBox.button.value;
+//   let parsedButtons = parseInt(value);
+//   let numButton = document.calcBox.screen.value += parsedButtons;
+//
+//
+// let allButtons = document.querySelectorAll()
+//
+//
+// let values = []
+//
+//
+// // create a variable to hold button value in an array and loop through
+//
+// let arrayValues = document.getElementByClass()
+
+// add innerHTML to screen
+let screenEl = document.querySelector("h2");
+screenEl.innerHTML = "WooHoo!";
+
+function updateDisplay(num) {
+  screenEl.innerHTML = num;
+}
