efe2cf0de3ee0a470540435064240c2efac7381b
Emily Deans <emilyldeans@gmail.com>
2017-07-16 10:29:31 -0400

    Added hover and clear function.

--- main.js
+++ main.js
@@ -1,47 +1,14 @@
-// // variables for the 'clear' and 'screen' blocks
-// let screen = document.getElementById("screen");
-// let clear = document.getElementById("clear");
-//
-//
-// function buttonValue() {
-//   for(let i = 0; i < )
-// }
-//
-//   let value = document.calcBox.button.value;
-//   let parsedButtons = parseInt(value);
-//   let numButton = document.calcBox.screen.value += parsedButtons;
-//
-//
-// let allButtons = document.querySelectorAll()
-//
-//
-// let values = []
-//
-//
-// // create a variable to hold button value in an array and loop through
-//
+
 // let arrayValues = document.getElementByClass()
 
 //
 // function updateDisplay(num) {
 //   screenEl.innerHTML = num;
 // }
-//
-// // adding 'click' event listener to buttons
-// let buttonFunction = document.querySelectorAll("button");
-//
-//
-// buttonFunction.addEventListener("click", buttonValue);
-//
+
 // function buttonValue(newNum) {
 //   let buttonNumber = buttonFunction.value;
 //   screenEl.innerHTML = buttonNumber;
-// }
-
-
-
-// document.getElementByClass("button").onclick = function() {
-//
 // }
 
 
@@ -53,29 +20,39 @@ let length = num.length;
 
 for(let i = 0; i < length; i++) {
   num[i].addEventListener("click", function() {
-    let number = this.value;
+    // could also use 'this.value' instead of num[i]
+    let number = num[i].value;
     let display = screen.innerHTML += number;
   }
-
-)
-}
+)};
 
 let operators = document.querySelectorAll("operator");
 let opLength = operators.length;
 
 for(let t = 0; t < opLength; t++) {
   opLength[i].addEventListener("click", function(){
-    let opInputs = this.value;
+  // could also use 'this.value' instead of num[i]
+    let opInputs = num[i].value;
     let display = screen.innerHTML += opInputs;
   }
+)};
 
-)
-}
+let clearValue = document.querySelector("#clear");
+clearValue.addEventListener("click", function() {
+  screen.innerHTML = "";
+});
 
-// document.querySelector("equal").addEventListener("click" function() {
-//   if(screen.innerHTML === output) {
-//     screen.innerHTML = eval(output);
-//   } else {
-//     screen.innerHTML = "";
-//   }
-// } );
+let evaluate = document.querySelector("#equal");
+evaluate.addEventListener("click", function(){
+  let finalVal = eval(screen);
+  let parseVal = parseInt(finalVal);
+  let display = screen.innerHTML += finalVal;
+  // console.log(finalVal);
+  // let display = parseInt(finalVal);
+  // screen.innerHTML = eval(finalVal);
+  // parseInt(screen);
+  //
+  // let display = eval(screen.innerHTML);
+  // let totalVal = display += "totalVal";
+  // screen.innerHTML = totalVal;
+});
