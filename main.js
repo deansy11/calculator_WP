2c56343cc613569701a5ab50c275eedc30669f1d
Emily Deans <emilyldeans@gmail.com>
2017-07-19 13:44:21 -0400

    Adjusted the '=' function to achieve desired output

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
-
-// let arrayValues = document.getElementByClass()
-
-//
-// function updateDisplay(num) {
-//   screenEl.innerHTML = num;
-// }
-
-// function buttonValue(newNum) {
-//   let buttonNumber = buttonFunction.value;
-//   screenEl.innerHTML = buttonNumber;
-// }
-
-
-// let screen = document.getElementById("screen");
 let screen = document.querySelector("h2");
-let num = document.querySelectorAll("button");
+let num = document.querySelectorAll(".button");
 
 let length = num.length;
 
@@ -22,18 +7,18 @@ for(let i = 0; i < length; i++) {
   num[i].addEventListener("click", function() {
     // could also use 'this.value' instead of num[i]
     let number = num[i].value;
-    let display = screen.innerHTML += number;
+    screen.innerHTML += number;
   }
 )};
 
-let operators = document.querySelectorAll("operator");
+let operators = document.querySelectorAll(".operator");
 let opLength = operators.length;
 
 for(let t = 0; t < opLength; t++) {
-  opLength[i].addEventListener("click", function(){
+  operators[t].addEventListener("click", function(){
   // could also use 'this.value' instead of num[i]
-    let opInputs = num[i].value;
-    let display = screen.innerHTML += opInputs;
+    let opInputs = operators[t].value;
+    screen.innerHTML += opInputs;
   }
 )};
 
@@ -42,18 +27,10 @@ clearValue.addEventListener("click", function() {
   screen.innerHTML = "";
 });
 
-// I left this trying to figure out how to create a function that will evaluate the 'screen.innerHTML' to the total and then will display that value on the screen. I kept getting totals that equaled [object] so I tried using parseInt on the value but then the screen returned 'NAN'
 let evaluate = document.querySelector("#equal");
-evaluate.addEventListener("click", function(){
-  let finalVal = eval(screen);
-  let parseVal = parseInt(finalVal);
-  screen.innerHTML = parseVal;
-  // console.log(finalVal);
-  // let display = parseInt(finalVal);
-  // screen.innerHTML = eval(finalVal);
-  // parseInt(screen);
-  //
-  // let display = eval(screen.innerHTML);
-  // let totalVal = display += "totalVal";
-  // screen.innerHTML = totalVal;
+evaluate.addEventListener("click", function(e){
+  e.preventDefault();
+  let screenTotal = screen.textContent;
+  let finalVal = eval(screenTotal);
+  screen.innerHTML = finalVal;
 });
